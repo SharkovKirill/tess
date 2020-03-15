db51b94653502aefccd3d531dad4812d3b197ceb
SharkovKirill <kirill.office75@gmail.com>
2020-03-15 13:58:13 +0700

    todo test

---
+++ tess.py
@@ -0,0 +1,89 @@
+"""Case-study #5 Тесселяция
+Разработчики:
+Шарков К., Кеда С., Ермоленко В:
+"""
+import math
+import turtle
+
+def get_color_choice():
+    while True:
+        colors = ['зеленый', 'красный', 'оранжевый', 'синий', 'бирюзовый', 'малиновый', 'индиго']
+        color = input("Допустимые цвета заливки(введите 1-7)\n"
+                      "1. Зеленый \n"
+                      "2. Красный \n"
+                      "3. Оранжевый \n"
+                      "4. Синий \n"
+                      "5. Бирюзовый \n"
+                      "6. Малиновый \n"
+                      "7. Индиго \n"
+                      "Пожалуйста, введите цвет: \n")
+        if color.lower() in colors:
+            if color == 'зеленый':
+                return 'green'
+            if color == 'красный':
+                return 'red'
+            if color == 'оранжевый':
+                return 'orange'
+            if color == 'синий':
+                return 'midnightblue'
+            if color == 'бирюзовый':
+                return 'turquoise'
+            if color == 'малиновый':
+                return 'crimson'
+            if color == 'индиго':
+                return 'indigo'
+            break
+        print('%s не является верным значением. Пожалуйста повторите попытку.' % color)
+
+def get_num_hexagons():
+    pass                        #4-20  return n
+
+def draw_hexagon(x, y, side_len, color):
+    pass
+
+def findparametrs(side_len, n):
+    parametrs = []
+    for vertik in range(1, n+1):
+        if vertik == 1:
+            x0 = math.sqrt(3) * side_len
+            y0 = 0
+            parametrs.append([x0,y0])
+        for gorizont in range(2, n+1):
+            x0 += math.sqrt(3) * side_len
+            parametrs.append([x0, y0])
+            if gorizont == n and vertik != n:       #[x0,y0] в parametrs - координаты верхних углов всех шестиугольников
+                if vertik % 2 == 1:
+                    y0 += 1.5 * side_len
+                    x0 = 0
+                    parametrs.append([x0, y0])
+                elif vertik % 2 == 0:
+                    x0 = math.sqrt(3) * side_len
+                    y0 += 1.5 * side_len
+                    parametrs.append([x0, y0])
+    return parametrs
+
+def find_colors(color1, color2):
+    colors = []
+    #return colors                      #цвета послеовательно расположены в colors
+    pass
+
+
+def main():
+    #color1 = get_color_choice()
+    #color2 = get_color_choice()
+    #print(color1, color2)
+    get_num_hexagons()
+    n = 4    #пока нету get_num_hexagons()
+    side_len = 500 // ((n + 0.5) * math.sqrt(3))
+    params = findparametrs(side_len, n)
+    #print(params)
+    #streamlined_colors = find_colors(color1, color2)
+
+    #for quantity in range(0, (n*n)+1):
+    #   draw_hexagon(parametrs[quantity][0], parametrs[quantity][0], side_len, streamlined_colors[quantity])
+
+main()
+
+
+# TODO:(Sveta) defs: get_num_hexagons()
+# TODO:(Vova) defs: find_colors(color1, color2), draw_hexagon(x, y, side_len, color)(для одного шестиуголльника)
